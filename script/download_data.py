4a8a9d87938f1743ca6179e3cbcb9914d0885ff2
vinbal97 <vinith.balasingam@kartverket.no>
2025-11-12 15:34:52 +0100

    ready to download images

--- script/download_data.py
+++ script/download_data.py
@@ -1,10 +1,14 @@
 
 from bs4 import BeautifulSoup
 import requests
+import os
+import time
 
 class ImageScraper:
-    def __init__(self):
-        pass
+    def __init__(self, download_folder=r"C:\Users\balvin\Documents\Github\tilgjengelighet\bilder"):
+        if os.path.isdir(download_folder) != True:
+            os.mkdir(download_folder)
+        self.download_folder = download_folder
 
     def grab_image_urls(self, side=1):
         """Function to grab all image urls from the first page. Return a list with all image urls."""
@@ -27,31 +31,34 @@ class ImageScraper:
                     pages = int(split.split("=")[1])
         return urls[pages:], pages
     
-    def download_image(self, url, filename):
+    def download_image(self, url):
         """Function to download an image from a given url and save it with the specified filename."""
         payload = {}
         headers = {}
 
         response = requests.request("GET", url, headers=headers, data=payload)
-
+        filename = os.path.join(self.download_folder, url.split("/")[-1])
         with open(filename, "wb") as f:
             f.write(response.content)
+            
+    
+    def main(self):
+        """Run skript"""
+        start = time.time()
+        urls = []
+        for i in range(1, 92):
+            print("-- Starting on page: ", i)
+            url = self.grab_image_urls(side=i)
+            url, pages = self.grab_pages(url)
+            urls.extend(url)
+        uniq = list(set(urls))
+
+        for link in uniq:
+            self.download_image(link)
+        slutt = time.time()
+
+        print("Tidbruk: ", slutt-start)
 
 if __name__ == "__main__":
     scraper = ImageScraper()
-    urls = scraper.grab_image_urls(side=1)
-    urls, pages = scraper.grab_pages(urls)
-
-"""
-url = "https://data.kartverket.no/tilgjengelighet/tilgjengelighet_thumb/3118_TettstedParkeringsområde_20250916145208_t.jpg"
-
-payload = {}
-headers = {}
-
-response = requests.request("GET", url, headers=headers, data=payload)
-
-print(response.text)
-with open("image.jpg", "wb") as f:
-    f.write(response.content)
-
-"""
+    scraper.main()
